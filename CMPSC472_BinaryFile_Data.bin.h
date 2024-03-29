380047dc3bae316fd38547b5ab197422e9eefe73
Tyler Thompson <tylert123@yahoo.com>
2024-03-29 18:24:45 -0400

    Added .h file

--- CMPSC472_BinaryFile_Data.bin.h
+++ CMPSC472_BinaryFile_Data.bin.h
@@ -1,8 +1,14 @@
-//
-// Created by tyler on 3/27/2024.
-//
-
 #ifndef CMPSC_472_RDWRBINARY_FILE_CMPSC472_BINARYFILE_DATA_BIN_H
 #define CMPSC_472_RDWRBINARY_FILE_CMPSC472_BINARYFILE_DATA_BIN_H
 
-#endif //CMPSC_472_RDWRBINARY_FILE_CMPSC472_BINARYFILE_DATA_BIN_H
+#define FILENAME "C:/Temp/CmpSc472_BinaryFile_Data.bin"
+
+// Structure definition
+struct Data {
+    int id;
+    char name[MAX_NAME_LENGTH];
+    int value;
+};
+
+#endif CMPSC_472_RDWRBINARY_FILE_CMPSC472_BINARYFILE_DATA_BIN_H
+
--- main.c
+++ main.c
@@ -1,4 +1,23 @@
+//-----------------------------------------------------------
+// Tyler Thompson CMPSC 472 Spring 2024
+//
+// This application is a team oriented. The goal of this
+// specific file is to write binary to a file given
+// input. The other file will read the binary, in which
+// the output should match the input. A .h file is created
+// to ensure that there is synchronization between the files.
+//-----------------------------------------------------------
+
 #include <stdio.h>
+#include "CMPSC472_BinaryFile_Data.bin.h"
+
+void writeToFileBinary() {
+    FILE *filePtr;
+    filePtr = fopen(FILENAME, "wb");
+    if (filePtr == NULL) {
+        printf("An error occurred when opening the file..\n");
+        exit(1);
+    }
 
 int main() {
     printf("Hello, World!\n");
